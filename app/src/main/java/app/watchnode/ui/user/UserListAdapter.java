4ab3b67107c65bff6f9a4c789549c777505762fa
Akalanka <akalankaperera128@gmail.com>
2022-10-23 12:21:04 +0530

    Fixed user delete and listview scroll

--- app/src/main/java/app/watchnode/ui/user/UserListAdapter.java
+++ app/src/main/java/app/watchnode/ui/user/UserListAdapter.java
@@ -1,14 +1,25 @@
 package app.watchnode.ui.user;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+
 import java.util.ArrayList;
 import app.watchnode.R;
+import app.watchnode.data.user.UserRepository;
 import app.watchnode.data.user.model.User;
+import app.watchnode.ui.login.LoginActivity;
 
 public class UserListAdapter extends ArrayAdapter<User> {
 
@@ -18,6 +29,7 @@ public class UserListAdapter extends ArrayAdapter<User> {
     private static class ViewHolder {
         TextView txtName;
         TextView txtEmail;
+        ImageButton deleteBtn;
     }
 
     public UserListAdapter(ArrayList<User> data, Context context) {
@@ -40,6 +52,7 @@ public class UserListAdapter extends ArrayAdapter<User> {
             convertView = inflater.inflate(R.layout.user_item, parent, false);
             viewHolder.txtName = (TextView) convertView.findViewById(R.id.list_item_user_name);
             viewHolder.txtEmail = (TextView) convertView.findViewById(R.id.list_item_user_email);
+            viewHolder.deleteBtn = (ImageButton) convertView.findViewById(R.id.btn_delete_usr);
 
             result=convertView;
 
@@ -48,8 +61,14 @@ public class UserListAdapter extends ArrayAdapter<User> {
             viewHolder = (UserListAdapter.ViewHolder) convertView.getTag();
             result=convertView;
         }
+        UserViewModel userViewModel = new UserViewModel(UserRepository.getInstance());
         viewHolder.txtName.setText(dataModel.getName());
         viewHolder.txtEmail.setText(dataModel.getEmail());
+        viewHolder.deleteBtn.setOnClickListener(v -> {
+            userViewModel.deleteUser(dataModel.getId());
+            this.dataSet.remove(dataModel);
+            this.notifyDataSetChanged();
+        });
         return convertView;
     }
 }
